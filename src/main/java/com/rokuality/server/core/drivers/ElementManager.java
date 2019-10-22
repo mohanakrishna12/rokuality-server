704ce580814928c2db1aed841bc0cb0750254e4e
Brandon Clark <baclark77@Brandons-MacBook-Pro-2.local>
2019-10-22 19:20:25 -0400

    concurrency performance improvements

--- src/main/java/com/rokuality/server/core/drivers/ElementManager.java
+++ src/main/java/com/rokuality/server/core/drivers/ElementManager.java
@@ -1,14 +1,17 @@
 package com.rokuality.server.core.drivers;
 
-import org.json.simple.JSONObject;
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import com.rokuality.server.constants.SessionConstants;
 
+import org.json.simple.JSONObject;
+
 public class ElementManager {
 
-	private static List<Map<String, JSONObject>> elementMap = Collections.synchronizedList(new ArrayList<>());
+	private static Queue<Map<String, JSONObject>> elementMap = new ConcurrentLinkedQueue<>();
 
 	public static void addElement(String sessionID, JSONObject element) {
 		Map<String, JSONObject> map = new HashMap<>();
@@ -17,21 +20,16 @@ public class ElementManager {
 	}
 
 	public static void removeElement(String sessionID) {
-		Map<String, JSONObject> map;
-		for (int i = 0; i < elementMap.size(); i++) {
-			map = elementMap.get(i);
+		for (Map<String, JSONObject> map : elementMap) {
 			if (map.containsKey(sessionID)) {
-				elementMap.remove(i);
+				elementMap.remove(map);
 				break;
 			}
 		}
 	}
 
 	public static JSONObject getElement(String sessionID, String elementID) {
-		Map<String, JSONObject> map;
-		for (int i = 0; i < elementMap.size(); i++) {
-			map = elementMap.get(i);
-
+		for (Map<String, JSONObject> map : elementMap) {
 			if (map.containsKey(sessionID) && map.get(sessionID).get(SessionConstants.ELEMENT_ID).equals(elementID)) {
 				return map.get(sessionID);
 			}
