234e7765e8fa5acc0a9885d1f2118bfefe4d8dbe
Brandon Clark <baclark77@Brandons-MacBook-Pro-2.local>
2019-12-21 15:44:56 -0500

    allow for launching a live channel

--- src/main/java/com/rokuality/server/core/ImageCollector.java
+++ src/main/java/com/rokuality/server/core/ImageCollector.java
@@ -162,7 +162,7 @@ public class ImageCollector {
 		}
 
 		// check if the device is hittable and the app is launched, allowing capturing
-		if (PlatformType.ROKU.equals(platform) && !RokuPackageHandler.isAppLaunched(deviceIP, "dev")) {
+		if (PlatformType.ROKU.equals(platform) && RokuPackageHandler.isAppLaunched(deviceIP, "Roku")) {
 			return null;
 		}
 
