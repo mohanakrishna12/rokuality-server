82eb8504b002734f322c4f93217236abdfd7e029
Brandon Clark <baclark77@Brandons-MacBook-Pro-2.local>
2019-12-29 13:16:09 -0500

    curl debugging

--- src/main/java/com/rokuality/server/driver/device/roku/RokuDevConsoleManager.java
+++ src/main/java/com/rokuality/server/driver/device/roku/RokuDevConsoleManager.java
@@ -1,316 +1,61 @@
 package com.rokuality.server.driver.device.roku;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.MalformedChallengeException;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.auth.DigestScheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.io.*;
+
+import com.rokuality.server.core.CommandExecutor;
+import com.rokuality.server.utils.OSUtils;
+
 import org.eclipse.jetty.util.log.Log;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.rokuality.server.enums.SessionCapabilities;
-
 public class RokuDevConsoleManager {
-
-	private static final int DEFAULT_TIMEOUT = 60;
-	// TODO - clean up the spaghetti exception code below with concurrent exception
-	// handling
 
 	private String username = "";
 	private String password = "";
 	private String deviceip = "";
+	private CommandExecutor commandExecutor = null;
+	private String curlPath = "";
 
 	public RokuDevConsoleManager(String deviceip, String username, String password) {
 		this.username = username;
 		this.password = password;
 		this.deviceip = deviceip;
+		this.commandExecutor = new CommandExecutor();
+		this.curlPath = OSUtils.getBinaryPath("curl");
+		if (!new File(this.curlPath).exists()) {
+			Log.getRootLogger().info("DEBUG - CURL DOES NOT EXIST!");
+		}
 	}
 
 	public boolean installRokuApp(String appPackage) {
-		String uri = getEndpointUri(deviceip, "plugin_install");
-		File appPackageFile = new File(appPackage);
-
-		Log.getRootLogger().info("Installing Roku app package '" + appPackage + "' at console url: " + uri,
-				new Object[] {});
-		boolean installSuccess = false;
-		try {
-			HttpHost target = getTarget(deviceip);
-			Header digestAuthHeader = getAuthHeader(deviceip, "plugin_install");
-
-			if (digestAuthHeader == null) {
-				Log.getRootLogger()
-						.warn(String.format(
-								"Failed to get authentication header during app install! "
-										+ "Is your device connected and listening at the provided %s capability?",
-								SessionCapabilities.DEVICE_IP_ADDRESS.value()));
-				return false;
-			}
-
-			CredentialsProvider credsProvider = getCredsProvider(target, username, password);
-
-			try (CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
-					.setDefaultRequestConfig(getRequestConfigTimeouts()).build()) {
-				AuthCache authCache = new BasicAuthCache();
-				DigestScheme digestAuth = new DigestScheme();
-				digestAuth.processChallenge(digestAuthHeader);
-				authCache.put(target, digestAuth);
-
-				HttpClientContext localContext = HttpClientContext.create();
-				localContext.setAuthCache(authCache);
-
-				HttpPost post = new HttpPost(uri);
-				HttpEntity entity = MultipartEntityBuilder.create().addTextBody("mysubmit", "Install")
-						.addBinaryBody("archive", appPackageFile, ContentType.create("application/zip"),
-								appPackageFile.getName())
-						.build();
-
-				post.setEntity(entity);
-
-				try (CloseableHttpResponse response = httpclient.execute(target, post, localContext)) {
-					int statusCode = response.getStatusLine().getStatusCode();
-					installSuccess = statusCode == 200;
-					if (!installSuccess) {
-						Log.getRootLogger().warn("Roku app install failed with response: " + response.getStatusLine()
-								+ System.lineSeparator()
-								+ EntityUtils.toString(response.getEntity()).replace(System.lineSeparator(), " "),
-								new Object[] {});
-					}
-				} catch (Exception e) {
-					Log.getRootLogger().warn(e);
-				}
-			} catch (MalformedChallengeException e) {
-				Log.getRootLogger().warn(e);
-			}
-		} catch (Exception e) {
-			Log.getRootLogger().warn(e);
-		}
-
-		return installSuccess;
+		String command = curlPath + " -u " + username + ":" + password + " -v -F mysubmit=Install -F 'archive=@" + appPackage +
+					"' http://" + deviceip + "/plugin_install --digest";
+		String output = commandExecutor.execCommand(command, null);
+		Log.getRootLogger().info("DEBUG - ROKU INSTALL OUTPUT: " + output);
+		return true; // TODO - just for debugging
 	}
 
 	public boolean uninstallRokuApp() {
-		String uri = getEndpointUri(deviceip, "plugin_install");
+		String command = curlPath + " -u " + username + ":" + password + " -v -F mysubmit=Delete -F 'archive= ' http://"
+				+ deviceip + "/plugin_install --digest";
 
-		Log.getRootLogger().info("Uninstalling sideloaded Roku app package", new Object[] {});
-		boolean uninstallSuccess = false;
-		try {
-			HttpHost target = getTarget(deviceip);
-			Header digestAuthHeader = getAuthHeader(deviceip, "plugin_install");
-
-			if (digestAuthHeader == null) {
-				Log.getRootLogger()
-						.warn(String.format(
-								"Failed to get authentication header during app uninstall! "
-										+ "Is your device connected and listening at the provided %s capability?",
-								SessionCapabilities.DEVICE_IP_ADDRESS.value()));
-				return false;
-			}
-
-			CredentialsProvider credsProvider = getCredsProvider(target, username, password);
-
-			try (CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
-					.setDefaultRequestConfig(getRequestConfigTimeouts()).build()) {
-				AuthCache authCache = new BasicAuthCache();
-				DigestScheme digestAuth = new DigestScheme();
-				digestAuth.processChallenge(digestAuthHeader);
-				authCache.put(target, digestAuth);
-
-				HttpClientContext localContext = HttpClientContext.create();
-				localContext.setAuthCache(authCache);
-
-				HttpPost post = new HttpPost(uri);
-				HttpEntity entity = MultipartEntityBuilder.create().addTextBody("mysubmit", "Delete")
-						.addTextBody("archive", " ").build();
-
-				post.setEntity(entity);
-
-				try (CloseableHttpResponse response = httpclient.execute(target, post, localContext)) {
-					int statusCode = response.getStatusLine().getStatusCode();
-					uninstallSuccess = statusCode == 200;
-					if (!uninstallSuccess) {
-						Log.getRootLogger().warn("Roku app uninstall failed with response: " + response.getStatusLine()
-								+ System.lineSeparator()
-								+ EntityUtils.toString(response.getEntity()).replace(System.lineSeparator(), " "),
-								new Object[] {});
-					}
-				} catch (Exception e) {
-					Log.getRootLogger().warn(e);
-				}
-			} catch (MalformedChallengeException e) {
-				Log.getRootLogger().warn(e);
-			}
-		} catch (Exception e) {
-			Log.getRootLogger().warn(e);
-		}
-
-		return uninstallSuccess;
+		String output = commandExecutor.execCommand(command, null);
+		Log.getRootLogger().info("DEBUG - ROKU UNINSTALL OUTPUT: " + output);
+		return true; // TODO - just for debugging purposes
 	}
 
 	public File getScreenshot(File fileToSaveAs) {
-		String uri = getEndpointUri(deviceip, "plugin_inspect");
+		String takeCommand = curlPath + " -u " + username + ":" + password
+						+ " -v -F mysubmit=Screenshot -F 'archive= ' -F 'passwd= '" + " http://" + deviceip
+						+ "/plugin_inspect --digest";
+		String output = commandExecutor.execCommand(takeCommand, null);
+		Log.getRootLogger().info("DEBUG - ROKU TAKE SCREENSHOT OUTPUT: " + output);
 
-		Log.getRootLogger().info("Getting screenshot from Roku.", new Object[] {});
-		String takeOutput = "";
-
-		Header digestAuthHeader = null;
-		CredentialsProvider credsProvider = null;
-		HttpHost target = null;
-		try {
-			target = getTarget(deviceip);
-			digestAuthHeader = getAuthHeader(deviceip, "plugin_inspect");
-			credsProvider = getCredsProvider(target, username, password);
-
-			try (CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
-					.setDefaultRequestConfig(getRequestConfigTimeouts()).build()) {
-				AuthCache authCache = new BasicAuthCache();
-				DigestScheme digestAuth = new DigestScheme();
-				digestAuth.processChallenge(digestAuthHeader);
-				authCache.put(target, digestAuth);
-
-				HttpClientContext localContext = HttpClientContext.create();
-				localContext.setAuthCache(authCache);
-
-				HttpPost post = new HttpPost(uri);
-				HttpEntity entity = MultipartEntityBuilder.create().addTextBody("mysubmit", "Screenshot")
-						.addTextBody("archive", " ").addTextBody("passwd", " ").build();
-
-				post.setEntity(entity);
-
-				try (CloseableHttpResponse response = httpclient.execute(target, post, localContext)) {
-					int statusCode = response.getStatusLine().getStatusCode();
-					takeOutput = EntityUtils.toString(response.getEntity());
-					boolean takeSuccess = statusCode == 200;
-					if (!takeSuccess) {
-						Log.getRootLogger().warn(
-								"Roku app screenshot take failed with response: " + response.getStatusLine(),
-								new Object[] {});
-					}
-				} catch (Exception e) {
-					Log.getRootLogger().warn(e);
-				}
-			} catch (MalformedChallengeException e) {
-				Log.getRootLogger().warn(e);
-			}
-		} catch (Exception e) {
-			Log.getRootLogger().warn(e);
-		}
-
-		File jpgFile = null;
-		try {
-			if (takeOutput.toLowerCase().contains("screenshot ok")) {
-				try {
-					try (CloseableHttpClient httpclient = HttpClients.custom()
-							.setDefaultRequestConfig(getRequestConfigTimeouts())
-							.setDefaultCredentialsProvider(credsProvider).build()) {
-						AuthCache authCache = new BasicAuthCache();
-						DigestScheme digestAuth = new DigestScheme();
-						digestAuth.processChallenge(digestAuthHeader);
-						authCache.put(target, digestAuth);
-
-						HttpClientContext localContext = HttpClientContext.create();
-						localContext.setAuthCache(authCache);
-
-						Long currentEpoch = (System.currentTimeMillis() - 1000) / 1000;
-						HttpGet get = new HttpGet(
-								"http://" + deviceip + "/pkgs/dev.jpg?time=" + currentEpoch.toString());
-
-						try (CloseableHttpResponse response = httpclient.execute(target, get, localContext)) {
-							int statusCode = response.getStatusLine().getStatusCode();
-							HttpEntity entity = response.getEntity();
-							InputStream inputStream = entity.getContent();
-
-							jpgFile = fileToSaveAs;
-							FileUtils.copyInputStreamToFile(inputStream, jpgFile);
-
-							if (statusCode != 200) {
-								Log.getRootLogger().warn(
-										"Roku app screenshot get failed with response: " + response.getStatusLine());
-								return null;
-							}
-
-							if (jpgFile.exists() && jpgFile.isFile()) {
-								return jpgFile;
-							}
-						} catch (Exception e) {
-							Log.getRootLogger().warn(e);
-						}
-					} catch (MalformedChallengeException e) {
-						Log.getRootLogger().warn(e);
-					}
-				} catch (Exception e) {
-					Log.getRootLogger().warn(e);
-				}
-			} else {
-				Log.getRootLogger().warn("Failed to get roku screenshot!");
-			}
-		} catch (Exception e) {
-			Log.getRootLogger().warn("Failed to capture roku screenshot!", e);
-		}
-
-		return null;
+		Long currentEpoch = (System.currentTimeMillis() - 3000) / 1000;
+		String getCommand = curlPath + " -u " + username + ":" + password + " http://" + deviceip
+						+ "/pkgs/dev.jpg?time=" + currentEpoch.toString() + " --digest > " + fileToSaveAs.getAbsolutePath();
+		output = commandExecutor.execCommand(getCommand, null);
+		Log.getRootLogger().info("DEBUG - ROKU GET SCREENSHOT OUTPUT: " + output);
+		return fileToSaveAs;
 	}
 
-	private static Header getAuthHeader(String deviceIP, String endpoint) {
-		String uri = getEndpointUri(deviceIP, endpoint);
-
-		Header digestAuthHeader = null;
-
-		try (CloseableHttpClient httpClient = HttpClientBuilder.create()
-				.setDefaultRequestConfig(getRequestConfigTimeouts()).build()) {
-			CloseableHttpResponse response = httpClient.execute(new HttpPost(uri));
-			digestAuthHeader = response.getFirstHeader("WWW-Authenticate");
-		} catch (Exception e) {
-			Log.getRootLogger().warn(e);
-		}
-
-		return digestAuthHeader;
-	}
-
-	private static String getBaseUri(String deviceIP) {
-		return "http://" + deviceIP;
-	}
-
-	private static String getEndpointUri(String deviceIP, String endpointName) {
-		return getBaseUri(deviceIP) + "/" + endpointName;
-	}
-
-	private static HttpHost getTarget(String deviceIP) throws MalformedURLException {
-		URL url = new URL(getBaseUri(deviceIP));
-		return new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
-	}
-
-	private static CredentialsProvider getCredsProvider(HttpHost target, String username, String password) {
-		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-		credsProvider.setCredentials(new AuthScope(target.getHostName(), target.getPort()),
-				new UsernamePasswordCredentials(username, password));
-		return credsProvider;
-	}
-
-	private static RequestConfig getRequestConfigTimeouts() {
-		int timeout = DEFAULT_TIMEOUT;
-		RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout * 1000)
-				.setConnectionRequestTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
-		return config;
-	}
 }
